45f8fbac9783e0de158468b8e6e9109a502e6359
coSK123 <62240598+coSK123@users.noreply.github.com>
2023-04-22 16:06:33 +0200

    Add files via upload

--- demo/src/main/java/com/example/demo/FenstringToBoard.java
+++ demo/src/main/java/com/example/demo/FenstringToBoard.java
@@ -1,33 +1,26 @@
 package com.example.demo;
 
 public class FenstringToBoard {
-    private String fenstring;
 
-    private static char[][] chessBoard = new char[8][8];
+    private static final char[][] chessBoard = new char[8][8];
 
 
     //8/8/4Rp2/5P2/1PP1pkP1/7P/1P1r4/7K b - - 0 40
-    public static void boardBuilder(String fenstring){
+    public static char[][] boardBuilder(String fenstring){
         int column = 0;
         int line = 0;
         for (char character: fenstring.toCharArray()) {
             switch (character) {
-                case '8', '7', '6', '5', '4', '3', '2', '1': {
+                case '8', '7', '6', '5', '4', '3', '2', '1' -> {
                     for (int i = 0; i < Character.getNumericValue(character); i++) {
                         chessBoard[column][line++] = 'e';
                     }
-                    break;
                 }
-                case 'R', 'r', 'K', 'k', 'Q', 'q', 'B', 'b', 'N', 'n', 'P', 'p': {
-                    chessBoard[column][line++] = character;
-                    break;
-                }
-                case '/': {
+                case 'R', 'r', 'K', 'k', 'Q', 'q', 'B', 'b', 'N', 'n', 'P', 'p' -> chessBoard[column][line++] = character;
+                case '/' -> {
                     line = 0;
                     column++;
-                    break;
                 }
-
             }
 
         }
@@ -37,11 +30,12 @@ public class FenstringToBoard {
                 }
                 System.out.println();
             }
+            return chessBoard;
     }
 
 
 
     public static void main(String[] args) {
         boardBuilder("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
-    }
+    } // rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR
 }
